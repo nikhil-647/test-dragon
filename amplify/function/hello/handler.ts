5bcbabcc7dc81504240f84ade2d3cc59135214bd
nikhil-647 <nkhlpatil647fake@gmail.com>
2025-06-06 08:01:33 +0530

    custom env variables

--- amplify/function/hello/handler.ts
+++ amplify/function/hello/handler.ts
@@ -1,7 +1,9 @@
 import { Schema } from "../../data/resource";
 type HandlerType = Schema["sayHello"]["functionHandler"];
+import { env } from "$amplify/env/sayHello";
 
 export const handler: HandlerType = async (event, context: any) => {
+    const { MY_REGION, MY_TABLE_NAME } = env;
     const { name } = event.arguments;
-    return `Hello, ${name} Dragon!`;
+    return `Hello, ${name} Dragon! ${MY_REGION} ${MY_TABLE_NAME}`;
 };
--- amplify/function/hello/resource.ts
+++ amplify/function/hello/resource.ts
@@ -1,9 +1,16 @@
 import { defineFunction } from "@aws-amplify/backend";
 
+const MY_REGION = 'us-east-1';
+const MY_TABLE_NAME = 'Product';
+
 export const sayHello = defineFunction({
   name: "sayHello",
   entry: "./handler.ts",
   bundling: {
     minify: false,
-  }
+  },
+  environment: {
+    MY_REGION,
+    MY_TABLE_NAME,
+  },
 });
